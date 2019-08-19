0502dffef2d1b347167663d298936453b670728e
trenyik <trenyik.nikolett1@gmail.com>
2019-08-19 15:40:03 +0100

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,3 @@
+CREATE TABLE IF NOT EXISTS projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date INTEGER, end_date INTEGER);
+CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
+CREATE TABLE IF NOT EXISTS pledges (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, amount INTEGER, user_id INTEGER, project_id INTEGER);
